c57353e362b5c687bfbc554a0f80455d035eabea
IvanDarma-S <ivandarma3000@gmail.com>
2026-05-17 15:30:10 +0700

    menambahkan kode di cart_repository.dart

--- lib/order/data/models/order_model.dart
+++ lib/order/data/models/order_model.dart
@@ -1,11 +1,16 @@
-import 'package:market_chart/features/cart/data/models/cart_model.dart';
-
 class OrderItemModel {
   final int productId;
   final String productName;
   final double price;
   final int quantity;
   final double subtotal;
+  OrderItemModel({
+    required this.productId,
+    required this.productName,
+    required this.price,
+    required this.quantity,
+    required this.subtotal,
+  });
 }
 
 class OrderModel {
@@ -18,4 +23,14 @@ class OrderModel {
   final String paymentMethod; // 'gopay' | 'bank_transfer' | 'virtual_account'
   final List<OrderItemModel> items;
   final String createdAt;
+  OrderModel({
+    required this.id,
+    required this.totalAmount,
+    required this.status,
+    required this.shippingAddress,
+    required this.notes,
+    required this.paymentMethod,
+    required this.items,
+    required this.createdAt,
+  });
 }
